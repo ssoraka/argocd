4b53c3001467cf9adc5cd18870e271815aed5606
root <ssoraka@student.21-school.ru>
2022-02-22 21:30:51 +0300

    .git

--- p3/scripts/gitlab.sh
+++ p3/scripts/gitlab.sh
@@ -16,5 +16,5 @@ kubectl create namespace gitlab
 curl https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml | sed -e 's/\( *\)- argocd-server/&\n\1- --insecure/' | kubectl apply -n argocd -f -
 sleep 3
 kubectl apply -f /vagrant/confs/ingress.yaml
-kubectl apply -f /vagrant/confs/argo_gitlab.yaml
 kubectl apply -f /vagrant/confs/gitlab.yaml
+kubectl apply -f /vagrant/confs/argo_gitlab.yaml
