a8e68c88a67078331408c55b6e1d69e96128b5d7
root <ssoraka@student.21-school.ru>
2022-02-23 22:44:53 +0300

    clear project

--- p3/scripts/cluster.sh
+++ p3/scripts/cluster.sh
@@ -7,11 +7,7 @@ k3d cluster create mycluster --servers 1 --agents 0 -p "8080:80@loadbalancer" -p
 kubectl create namespace argocd
 kubectl create namespace dev
 
-#
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
 # for an IngressRoute type
-# kubectl wait --for=condition=complete --timeout=300s -n kube-system job.batch/helm-install-traefik-crd
 curl https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml | sed -e 's/\( *\)- argocd-server/&\n\1- --insecure/' | kubectl apply -n argocd -f -
 sleep 3
 kubectl apply -f /vagrant/confs/ingress.yaml
